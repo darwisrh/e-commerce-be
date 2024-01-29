933797f361a99862ed10500ef6f332d69d7f27eb
Darwis <107021419+darwisrh@users.noreply.github.com>
2024-01-29 20:43:00 +0700

    migration & model version 0.0.1

---
+++ prisma/migrations/20240129133614_version_0_0_1/migration.sql
@@ -0,0 +1,217 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "otp" TEXT,
+    "is_verify" BOOLEAN NOT NULL,
+    "age" INTEGER,
+    "date_birth" TIMESTAMP(3),
+    "gender" BOOLEAN,
+    "phone_number" INTEGER,
+    "address" TEXT,
+    "photo_profile" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "id_role" INTEGER NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "shops" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "photo_profile" TEXT,
+    "opened_at" TIMESTAMP(3) NOT NULL,
+    "closed_at" TIMESTAMP(3) NOT NULL,
+    "operasional_desc" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "id_admin" INTEGER NOT NULL,
+    "id_type" INTEGER NOT NULL,
+
+    CONSTRAINT "shops_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "cashback" INTEGER,
+    "cashback_total" INTEGER,
+    "sold" INTEGER,
+    "is_special_edition" BOOLEAN NOT NULL,
+    "images" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "id_shop" INTEGER NOT NULL,
+    "id_category" INTEGER NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_details" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "spec_name" TEXT NOT NULL,
+    "spec" TEXT NOT NULL,
+    "id_product" INTEGER NOT NULL,
+
+    CONSTRAINT "product_details_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "carts" (
+    "id" SERIAL NOT NULL,
+    "product_name" TEXT NOT NULL,
+    "product_detail" TEXT NOT NULL,
+    "total" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "id_user" INTEGER NOT NULL,
+
+    CONSTRAINT "carts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" SERIAL NOT NULL,
+    "product_name" TEXT NOT NULL,
+    "product_detail" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" BIGINT NOT NULL,
+    "total" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "id_status" INTEGER NOT NULL,
+    "id_user" INTEGER NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" SERIAL NOT NULL,
+    "commnet" TEXT NOT NULL,
+    "images" TEXT,
+    "id_user" INTEGER NOT NULL,
+    "id_other_user" INTEGER,
+    "id_product" INTEGER NOT NULL,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "etalase" (
+    "id" SERIAL NOT NULL,
+    "etalase_name" TEXT NOT NULL,
+    "id_shop" INTEGER NOT NULL,
+
+    CONSTRAINT "etalase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ratings" (
+    "id" SERIAL NOT NULL,
+    "rate" INTEGER NOT NULL,
+    "id_product" INTEGER NOT NULL,
+    "id_user" INTEGER NOT NULL,
+
+    CONSTRAINT "ratings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mst_order_status" (
+    "id" SERIAL NOT NULL,
+    "status_name" TEXT NOT NULL,
+
+    CONSTRAINT "mst_order_status_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mst_roles" (
+    "id" SERIAL NOT NULL,
+    "role_name" TEXT NOT NULL,
+
+    CONSTRAINT "mst_roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mst_shop_types" (
+    "id" SERIAL NOT NULL,
+    "type_name" TEXT NOT NULL,
+    "max_product" INTEGER NOT NULL,
+
+    CONSTRAINT "mst_shop_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mst_categories" (
+    "id" SERIAL NOT NULL,
+    "category_name" TEXT NOT NULL,
+
+    CONSTRAINT "mst_categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "shops_id_admin_key" ON "shops"("id_admin");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "comments_id_other_user_key" ON "comments"("id_other_user");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_id_role_fkey" FOREIGN KEY ("id_role") REFERENCES "mst_roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "shops" ADD CONSTRAINT "shops_id_admin_fkey" FOREIGN KEY ("id_admin") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "shops" ADD CONSTRAINT "shops_id_type_fkey" FOREIGN KEY ("id_type") REFERENCES "mst_shop_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_id_shop_fkey" FOREIGN KEY ("id_shop") REFERENCES "shops"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_id_category_fkey" FOREIGN KEY ("id_category") REFERENCES "mst_categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_details" ADD CONSTRAINT "product_details_id_product_fkey" FOREIGN KEY ("id_product") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_id_status_fkey" FOREIGN KEY ("id_status") REFERENCES "mst_order_status"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_id_other_user_fkey" FOREIGN KEY ("id_other_user") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_id_product_fkey" FOREIGN KEY ("id_product") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "etalase" ADD CONSTRAINT "etalase_id_shop_fkey" FOREIGN KEY ("id_shop") REFERENCES "shops"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ratings" ADD CONSTRAINT "ratings_id_product_fkey" FOREIGN KEY ("id_product") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ratings" ADD CONSTRAINT "ratings_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

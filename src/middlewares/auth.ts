cda9c391ff850fc41fd98bf3f4b6f420a8572c9d
Darwis <107021419+darwisrh@users.noreply.github.com>
2024-02-24 15:28:57 +0700

    fixed auth jsonwebtoken

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from "express"
+import { Response, NextFunction } from "express"
 import * as jwt from "jsonwebtoken"
 
-export function auth(req: Request, res: Response, next: NextFunction): Response | undefined {
+export function auth(req: any, res: Response, next: NextFunction): Response | undefined {
    const authHeader: string | undefined = req.header("Authorization")
    const token: string | undefined = authHeader && authHeader.split(" ")[1]
 
@@ -13,8 +13,14 @@ export function auth(req: Request, res: Response, next: NextFunction): Response 
 
    try {
       const secret: string = process.env.SECRET_KEY || "secret"
-      jwt.verify(token, secret)
-      next()
+      jwt.verify(token, secret, (err: any): Response | undefined => {
+         if (err) {
+            return res.status(403).send({
+               message: "Invalid token"
+            })
+         }
+         next()
+      })
    } catch (error) {
       next(error)
       res.send({
